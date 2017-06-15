d1ea00a4252fbb7494ad5fc4ce4cbf3402c291a8
jilldela <jilldelosangeles@gmail.com>
2017-06-15 08:43:08 -0700

    Fix contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@
 
 (function($) {
 
+	
+
 	// Settings.
 		var settings = {
 
@@ -626,6 +628,128 @@
 								);
 
 					});
+
+			//Resume
+				$('#resume')
+					.on('click', 'a', function(event) {
+
+						var $a = $(this),
+							$resume = $a.parents('#resume'),
+							$modal = $resume.children('.modal'),
+							$modalImg = $modal.find('img'),
+							href = $a.attr('href');
+
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Locked? Bail.
+							if ($modal[0]._locked)
+								return;
+
+						// Lock.
+							$modal[0]._locked = true;
+
+						// Set src.
+							$modalImg.attr('src', href);
+
+						// Set visible.
+							$modal.addClass('visible');
+
+						// Focus.
+							$modal.focus();
+
+						// Delay.
+							setTimeout(function() {
+
+								// Unlock.
+									$modal[0]._locked = false;
+
+							}, 600);
+					})
+					.on('click', '.modal', function(event) {
+
+						var $modal = $(this),
+							$modalImg = $modal.find('img');
+
+						// Locked? Bail.
+							if ($modal[0]._locked)
+								return;
+
+						// Already hidden? Bail.
+							if (!$modal.hasClass('visible'))
+								return;
+
+						// Stop propagation.
+							event.stopPropagation();
+
+						// Lock.
+							$modal[0]._locked = true;
+
+						// Clear visible, loaded.
+							$modal
+								.removeClass('loaded')
+
+						// Delay.
+							setTimeout(function() {
+
+								$modal
+									.removeClass('visible')
+
+								// Pause scroll zone.
+									$wrapper.triggerHandler('---pauseScrollZone');
+
+								setTimeout(function() {
+
+									// Clear src.
+										$modalImg.attr('src', '');
+
+									// Unlock.
+										$modal[0]._locked = false;
+
+									// Focus.
+										$body.focus();
+
+								}, 475);
+
+							}, 125);
+
+					})
+					.on('keypress', '.modal', function(event) {
+
+						var $modal = $(this);
+
+						// Escape? Hide modal.
+							if (event.keyCode == 27)
+								$modal.trigger('click');
+
+					})
+					.on('mouseup mousedown mousemove', '.modal', function(event) {
+
+						// Stop propagation.
+							event.stopPropagation();
+
+					})
+					.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
+						.find('img')
+							.on('load', function(event) {
+
+								var $modalImg = $(this),
+									$modal = $modalImg.parents('.modal');
+
+								setTimeout(function() {
+
+									// No longer visible? Bail.
+										if (!$modal.hasClass('visible'))
+											return;
+
+									// Set loaded.
+										$modal.addClass('loaded');
+
+								}, 275);
+
+							});
 
 			// Gallery.
 				$('.gallery')
